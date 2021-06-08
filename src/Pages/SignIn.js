6d8bafd646c6525d601ed15f2faba1dcf0f2b9e7
lokeshkumar3132 <kumarlokesh3132@gmail.com>
2021-06-08 16:10:49 +0530

    added sign In page and sign in with google/facebook

--- src/Pages/SignIn.js
+++ src/Pages/SignIn.js
@@ -1,10 +1,66 @@
 import React from 'react'
+import firebase from 'firebase/app'
+import {Container, Grid, Panel, Row, Col, Button, Icon, Alert} from 'rsuite'
+import { auth, database} from '../misc/firebase'
 
-const SignIn = () =>  (
-    <div>
-      hello
-    </div>
+
+const SignIn = () =>  {
+
+  const singInwithProvider = async (provider) => {
+
+    try{
+      const {additionalUserInfo, user} =await auth.signInWithPopup(provider);
+      if(additionalUserInfo.isNewUser){
+        await database.ref(`profiles/${user.uid}`).set({
+          name: user.displayName,
+          createdAt: firebase.database.ServerValue.TIMESTAMP
+        })
+      }
+      Alert.success('Signed In')
+
+    }catch(err){
+        Alert.error(err.message, 4000)
+        // eslint-disable-next-line no-console
+        console.log(err.message)
+    }
+    
+    
+  };
+
+  const onFacebook = () => {
+      singInwithProvider( new firebase.auth.FacebookAuthProvider() );
+  };
+
+  const onGoogle = () => {
+    singInwithProvider( new firebase.auth.GoogleAuthProvider() );
+  };
+  return(
+    <Container>
+      <Grid className="mt-page">
+        <Row>
+          <Col xs={24} md={12} mdOffset={6}>
+            <Panel>
+              <div className="text-center">
+                <h2>Welcome to Chat</h2>
+                <p>A progressive Chat-App for beginners</p>
+              </div>
+              <div className="mt-3">
+                <Button block color="blue" onClick={onFacebook}>
+                  <Icon icon="facebook" />
+                    Continue with Facebook
+                </Button>
+                <Button block color="green" onClick={onGoogle}>
+                  <Icon icon="google" />
+                  Continue with Google
+                </Button>
+              </div>
+            </Panel>
+          </Col>
+        </Row>
+      </Grid>
+    </Container>
   )
+}
 
 
 export default SignIn
--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,5 +1,6 @@
-import firebase from 'firebase/app'
-
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
   apiKey: "AIzaSyAw029cB1c36k-gEv1Gw2qF74HzarZvb1o",
@@ -11,3 +12,6 @@ const config = {
 };
 
 const app = firebase.initializeApp(config);
+
+export const auth =  app.auth();
+export const database = app.database();

366e3136970e37eb65b8b121adbcdd8f3656aa8f
lokeshkumar3132 <kumarlokesh3132@gmail.com>
2021-06-11 15:54:18 +0530

    added mediaQuery and responsiveness to drawer

--- src/Components/DashBoard/DashboardToggle.js
+++ src/Components/DashBoard/DashboardToggle.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Button, Icon, Drawer} from 'rsuite';
-import { useModelState } from '../../misc/custom-hooks';
+import { useMediaQuery, useModelState } from '../../misc/custom-hooks';
 import Dashboard from '.';
 
 const DashboardToggle = () => {
 
   const { isOpen, open, close } = useModelState()
+  const isMobile = useMediaQuery('max-width: 992px')
   return(
     <>
      <Button block color="blue" onClick={open}>
        <Icon icon="dashboard"/>
        Dashboard
        </Button> 
-       <Drawer show={isOpen} onHide={close} position="left">
+       <Drawer full={isMobile} show={isOpen} onHide={close} position="left">
         <Dashboard />
        </Drawer>
     </>

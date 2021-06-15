a5c5f05f0ebb1eaff42f52830e99a41930a1155a
lokeshkumar3132 <kumarlokesh3132@gmail.com>
2021-06-15 13:40:42 +0530

    started creating chat top; Room description chat modal created

--- src/Components/chat-window/top/index.js
+++ src/Components/chat-window/top/index.js
@@ -1,15 +1,31 @@
-/* eslint-disable arrow-body-style */
-import React from 'react'
+import React, { memo } from 'react'
+import { Icon, ButtonToolbar } from 'rsuite'
+import { Link } from 'react-router-dom';
 import { useCurrentRoom } from '../../../context/current-room.context'
+import { useMediaQuery } from '../../../misc/custom-hooks';
+import RoomInfoBtnMoal from './RoomInfoBtnModal'
 
 const ChatTop = () => {
 
   const name = useCurrentRoom(v => v.name);
+
+  const isMobile = useMediaQuery('(max-width: 992px)')
   return (
     <div>
-     {name} 
+      <div className="d-flex justify-content-between align-items-center">
+        <h4>
+          <Icon componentClass={Link} to="/" icon="arrow-circle-left" size="2x" className={isMobile ? 'd-inline-block p-0 mr-2 text-blue link-unstyled' : 'd-none'} />
+          <span className="text-disappear">  {name} </span>
+        </h4>  
+        <ButtonToolbar className="ws-nowrap">To do</ButtonToolbar>
+      </div>
+      <div className="d-flex justify-content-between align-items-center">
+        <span>Todo</span>
+        <RoomInfoBtnMoal />
+      </div>
+     
     </div>
   )
 }
 
-export default ChatTop
+export default memo(ChatTop)

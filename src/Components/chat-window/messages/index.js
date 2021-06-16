884fa5e9dcf0c0010edaf86b36148398d17586d1
lokeshkumar3132 <kumarlokesh3132@gmail.com>
2021-06-16 13:04:18 +0530

    displayedmessages iside chat page

--- src/Components/chat-window/messages/index.js
+++ src/Components/chat-window/messages/index.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router';
+import { database } from '../../../misc/firebase'
+import { transformToArray } from '../../../misc/helpers';
+import MessageItem from './MessageItem';
 
-// eslint-disable-next-line arrow-body-style
 const Messages = () => {
+
+  const { chatId } = useParams();
+  const [messages, setMessages] = useState(null);
+
+  const isChatEmpty = messages && messages.length === 0;
+  const canShowMessage = messages && messages.length > 0;
+
+  useEffect(() => {
+    
+    const messageRef = database.ref('/messages');
+
+    messageRef.orderByChild('roomId').equalTo(chatId).on('value', (snap) => {
+      const data = transformToArray(snap.val());
+
+      setMessages(data);
+
+    })
+    
+    return () => {
+      messageRef.off('value');
+    }
+
+  }, [chatId])
+
   return (
-    <div>
-     messages 
-    </div>
+    <ul className="msg-list custom-scroll">
+     {isChatEmpty && <li>No messages yet</li>} 
+     {canShowMessage && messages.map(msg => <MessageItem key={msg.id} message={msg}/>)}
+    </ul>
   )
 }
 
